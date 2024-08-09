8b478c0608028fdd52b7dc1945e2346cef212c27
InternPro Reference <116167850+InternProO@users.noreply.github.com>
2024-08-10 01:34:48 +0530

    Create Script.js

---
+++ Intermediate task /Task3/Script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    // Add custom JS here to handle user interactions, dynamic content, etc.
+    console.log('AR Web Framework initialized');
+});
